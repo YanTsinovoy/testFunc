3c113f31430c8adec46b2b1f44e41387b655020b
YanTsinovoy <yand199627@gmail.com>
2019-03-16 11:34:50 +0200

    Updates

---
+++ precache-manifest.cb9cede21b73b76cce5c8d436dd581fa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c8abf364d762d88093a5",
+    "url": "/testFunc/static/js/runtime~main.153cc0a4.js"
+  },
+  {
+    "revision": "0771c7b718e947c79dbb",
+    "url": "/testFunc/static/js/main.d125b667.chunk.js"
+  },
+  {
+    "revision": "bc0747153ff695b50ee7",
+    "url": "/testFunc/static/js/2.04d6c229.chunk.js"
+  },
+  {
+    "revision": "0771c7b718e947c79dbb",
+    "url": "/testFunc/static/css/main.d8398878.chunk.css"
+  },
+  {
+    "revision": "11eec6824be82164f6446b1342c96521",
+    "url": "/testFunc/index.html"
+  }
+];
